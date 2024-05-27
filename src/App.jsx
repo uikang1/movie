cd81a84b2af2b98549a648ec0b5a053d538c3dc9
uikang1 <158258162+uikang1@users.noreply.github.com>
2024-05-27 23:33:22 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import Movie from '../Movie/Movie';
+import MovieStart from '../Movie/MovieStart';
 import GlobalStyle from './styled/GlobalStyle';
+import { useState } from 'react';
 
 const App = () => {
+    const [showMovie, setShowMovie] = useState(false);
+    const [streetlampOn, setStreetlampOn] = useState(false);
+
+    const handleStreetlampClick = () => {
+        setStreetlampOn(true); // 가로등을 켜는 상태로 변경
+        setTimeout(() => {
+            setShowMovie(true);
+        }, 1000);
+    };
+
     return (
         <div>
-            <Movie />
+            {!showMovie && <MovieStart handleStreetlampClick={handleStreetlampClick} />}
+            {showMovie && <Movie />}
             <GlobalStyle />
+            {streetlampOn && (
+                <div className="streetlamp-on-animation">{/* 가로등이 켜지는 애니메이션 */}</div>
+            )}
         </div>
     );
 };
